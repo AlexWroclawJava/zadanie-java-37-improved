30ac6857e214025ad8213b7109b468a284b5c1ae
AlexWroclawJava <aleksander.szlachcic@gmail.com>
2018-02-08 09:25:09 +0100

    zadanie-java-37 wyjatki_niekontrolowane

---
+++ src/Person.java
@@ -0,0 +1,63 @@
+public class Person {
+
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String pesel;
+
+    public Person(String firstName, String lastName, int age, String pesel) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.pesel = pesel;
+    }
+
+    public Person() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        if (firstName == null) {
+            throw new NameUndefinedException();
+        }
+        if (firstName.length() < 2) {
+            throw new NameUndefinedException();
+        }
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        if (lastName == null) {
+            throw new NameUndefinedException();
+        }
+        if (lastName.length() <= 2) {
+            throw new NameUndefinedException();
+        }
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age < 1) {
+            throw new IncorrectAgeException();
+        }
+        this.age = age;
+    }
+
+    public String getPesel() {
+        return pesel;
+    }
+
+    public void setPesel(String pesel) {
+        this.pesel = pesel;
+    }
+}
---
+++ src/PersonWprowadz.java
@@ -0,0 +1,27 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class PersonWprowadz {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        Person person = new Person();
+
+        System.out.println("Podaj imię osoby: ");
+        person.setFirstName(scanner.nextLine());
+        System.out.println("Podaj nazwisko osoby: ");
+        person.setLastName(scanner.nextLine());
+        System.out.println("Podaj wiek osoby: ");
+        person.setAge(scanner.nextInt());
+        scanner.nextLine();
+        System.out.println("Podaj numer pesel osoby: ");
+        person.setPesel(scanner.nextLine());
+
+        System.out.println("\nWprowadzona osoba to: ");
+        System.out.println("Imie: " + person.getFirstName());
+        System.out.println("Nazwisko: " + person.getLastName());
+        System.out.println("Wiek: " + person.getAge());
+        System.out.println("Pesel: " + person.getPesel());
+    }
+}
